1f6096726dca88bb6f422a394fa43f7368ea8d45
yonePC <ys.mailaddress0929@gmail.com>
2021-08-16 01:51:33 +0000

    add users/show,edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,29 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+
   end
 
   def complete
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :number, :email, :image)
+  end
+
+
 end
