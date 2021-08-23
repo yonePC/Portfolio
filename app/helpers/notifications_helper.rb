5d41193948124772bd2071546d2fd3fd1178d138
yonePC <ys.mailaddress0929@gmail.com>
2021-08-23 06:00:15 +0000

    update notifications translate time_ago_in_words into japanese

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,12 +3,10 @@ module NotificationsHelper
       visiter = link_to notification.action_user.name, notification.action_user, style:"font-weight: bold;"
       your_post = link_to 'あなたの投稿', notification.post, style:"font-weight: bold;"
 
-
-      # @action_user_comment = notification.comment_id
-      @comment = Comment.find_by(id: notification.comment_id) #&.content
+      @comment = Comment.find_by(id: notification.comment_id)
       "#{visiter}さんが#{your_post}にコメントしました。"
    end
-   
+
    def unchecked_notifications
     @notifications = current_user.passive_notifications.where(check: false)
    end
