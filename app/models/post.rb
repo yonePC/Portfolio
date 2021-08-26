b7e5b6ff4ddd92060c121fcd6aa3219324fe5144
yonePC <ys.mailaddress0929@gmail.com>
2021-08-26 08:46:35 +0000

    make layout notifications/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -36,4 +36,24 @@ class Post < ApplicationRecord
       self.tags << post_tag
     end
   end
+
+  def create_notification_comment!(current_user, comment_id)
+    save_notification_comment!(current_user, comment_id, user_id)
+  end
+
+  # コメント通知
+  def save_notification_comment!(current_user, comment_id, ationed_user_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      ationed_user_id: ationed_user_id,
+      action: "comment"
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.action_user_id == notification.ationed_user_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
