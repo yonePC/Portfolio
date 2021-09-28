96d7f515b191a1a9e2361c75cff2641431b98017
yonePC <ys.mailaddress0929@gmail.com>
2021-09-28 11:16:55 +0000

    update error_messages translate into japanese

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
     has_many :tags, through: :post_tags
 
-  validates :area, presence: { message: "実施エリアを入力してください" }
-  validates :body, presence: { message: "内容を入力してください" }
+  validates :area, presence: true
+  validates :body, presence: true
 
   def stamped_by?(user) # スタンプが押されているかの判定
     stamps.where(user_id: user.id).exists?
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   has_many :active_notifications, class_name: "Notification", foreign_key: "action_user_id", dependent: :destroy
   has_many :passive_notifications, class_name: "Notification", foreign_key: "ationed_user_id", dependent: :destroy
 
-  validates :name, presence: { message: "名前を入力してください" }
-  validates :number, numericality: { message: "社員番号は数値で入力してください" }
-  # validates :email, presence: { message: "メールを入力してください" }
-  # validates :password, presence: { message: "パスワードを入力してください" }
+  validates :name, presence: true
+  validates :number, presence: true, numericality: true
 end
